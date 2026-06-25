d260e851253b9ae83081e01420293cec17d9aa60
wreed3 <91444925+wreed3@users.noreply.github.com>
2026-06-25 00:19:47 -0500

    add 3d gameboard with interactive cells and winning cell highlighting (#13)
    Files changed:
      - src/GameBoard3D.js (update)
      - src/Cell3D.js (update)

--- src/GameBoard3D.js
+++ src/GameBoard3D.js
@@ -10,39 +10,48 @@ function GameBoard3D({ board, onCellClick, winningCells }) {
   };
 
   return (
-    <Canvas style={{ height: '600px', background: '#0f172a' }}>
-      <PerspectiveCamera makeDefault position={[5, 5, 5]} />
-      <OrbitControls 
-        enablePan={true}
-        enableZoom={true}
-        enableRotate={true}
-        minDistance={3}
-        maxDistance={15}
-      />
-      
-      {/* Lighting */}
-      <ambientLight intensity={0.5} />
-      <pointLight position={[10, 10, 10]} intensity={1} />
-      <pointLight position={[-10, -10, -10]} intensity={0.5} />
+    <div style={{ width: '100%', height: '600px' }}>
+      <Canvas 
+        style={{ width: '100%', height: '100%' }}
+        gl={{ antialias: true, alpha: false }}
+        dpr={[1, 2]}
+      >
+        <color attach="background" args={['#0f172a']} />
+        <PerspectiveCamera makeDefault position={[5, 5, 5]} fov={50} />
+        <OrbitControls 
+          enablePan={true}
+          enableZoom={true}
+          enableRotate={true}
+          minDistance={3}
+          maxDistance={15}
+          target={[0, 0, 0]}
+        />
+        
+        {/* Lighting */}
+        <ambientLight intensity={0.6} />
+        <directionalLight position={[10, 10, 10]} intensity={1} />
+        <directionalLight position={[-10, -10, -10]} intensity={0.3} />
+        <pointLight position={[0, 5, 0]} intensity={0.5} />
 
-      {/* Render all 27 cells */}
-      {board.map((layer, z) =>
-        layer.map((row, y) =>
-          row.map((cell, x) => (
-            <Cell3D
-              key={`${z}-${y}-${x}`}
-              position={[(x - 1) * 1.2, (y - 1) * 1.2, (z - 1) * 1.2]}
-              value={cell}
-              onClick={() => onCellClick(z, y, x)}
-              isWinningCell={isWinningCell(z, y, x)}
-            />
-          ))
-        )
-      )}
+        {/* Render all 27 cells */}
+        {board.map((layer, z) =>
+          layer.map((row, y) =>
+            row.map((cell, x) => (
+              <Cell3D
+                key={`${z}-${y}-${x}`}
+                position={[(x - 1) * 1.2, (y - 1) * 1.2, (z - 1) * 1.2]}
+                value={cell}
+                onClick={() => onCellClick(z, y, x)}
+                isWinningCell={isWinningCell(z, y, x)}
+              />
+            ))
+          )
+        )}
 
-      {/* Grid helper for reference */}
-      <gridHelper args={[6, 6]} position={[0, -2, 0]} />
-    </Canvas>
+        {/* Grid helper for reference */}
+        <gridHelper args={[6, 6]} position={[0, -2, 0]} />
+      </Canvas>
+    </div>
   );
 }
 
